6d641b519625293d58e2951e7e7cccd90acab24d
unknown <Ivan>
2017-12-25 13:29:15 +0700

    init

---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  TOKEN: '360120353:AAF9zOHbHnwHz5zdpQ27dO1Ry65tdL5Bbn0'
+}
---
+++ src/keyboard.js
@@ -0,0 +1,24 @@
+const kb = require('./keyboard-buttons')
+
+module.exports = {
+  home: [
+    [kb.home.kursi, kb.home.pomosh]
+  ],
+    kursi: [
+        [kb.kursi.concentratic, kb.kursi.dolina],
+        [kb.kursi.zapusk, kb.kursi.nastavnik],
+        [kb.kursi.nazad]
+    ],
+    pomoshtwo: [
+        [kb.pomoshtwo.vopros],
+        [kb.pomoshtwo.nazad]
+    ],
+    pomosh: [
+        [kb.pomosh.vopros],
+        [kb.pomosh.nazad]
+        ],
+    telephone: [
+        [kb.telephone.phone]
+    ]
+
+}
